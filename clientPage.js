ec5f4e74c55f2ab6859a58fad08f379b57c765c7
sb39 <biswassrijeet99@gmail.com>
2018-10-30 13:42:41 +0530

    layout design complete

--- clientPage.js
+++ clientPage.js
@@ -75,3 +75,5 @@ angular.module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
 Copyright 2018 Google Inc. All Rights Reserved. 
 Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
 **/
+
+// map
