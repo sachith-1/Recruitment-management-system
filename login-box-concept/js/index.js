f1ed4f870010b5138fe55ef9220fc04d0b03a7ac
sb39 <biswassrijeet99@gmail.com>
2018-10-31 16:36:26 +0530

    login enabled

--- login-box-concept/js/index.js
+++ login-box-concept/js/index.js
@@ -30,9 +30,10 @@ $('input[type="text"],input[type="password"]').blur(function(){
 
 $('input[type="text"],input[type="password"]').keyup(function(){
   if(!$(this).val() == ''){
-    $(this).next().animate({'opacity':'0','right' : '30'},200)
+    $(this).next().animate({'opacity':'1','right' : '30'},200);
+    
   } else {
-    $(this).next().animate({'opacity':'1','right' : '20'},200)
+    $(this).next().animate({'opacity':'0','right' : '20'},200)
   }
 });
 
