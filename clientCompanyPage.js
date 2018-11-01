e82652f3ec0397fa660df7a432cb1746064d73d8
sb39 <biswassrijeet99@gmail.com>
2018-11-01 14:23:59 +0530

    top arrow added

--- clientCompanyPage.js
+++ clientCompanyPage.js
@@ -45,4 +45,23 @@ $('.tab').click(function(){
   });
 });
 /* /////  login and logout js //// */
+$(document).ready(function(){
+  $(window).scroll(function () {
+         if ($(this).scrollTop() > 1080) {
+             $('#back-to-top').fadeIn();
+         } else {
+             $('#back-to-top').fadeOut();
+         }
+     });
+     // scroll body to 0px on click
+     $('#back-to-top').click(function () {
+         $('#back-to-top').tooltip('hide');
+         $('body,html').animate({
+             scrollTop: 0
+         }, 800);
+         return false;
+     });
+     
+     $('#back-to-top').tooltip('show');
 
+});
